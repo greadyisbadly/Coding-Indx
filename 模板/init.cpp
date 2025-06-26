8cfec328e8c212c6aecc4db58ccd227e5f44a3e7
Rime <shishize0123@foxmail.com>
2025-06-26 19:51:13 +0800

    first commit

---
+++ 模板/init.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> P;
+
+const int N = 1e6 + 7, mod = 1e9 + 7, inf = 1e15;
+int n, m, k;
+
+inline void run(){
+}
+signed main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int T;
+    //for(cin >> T; T > 0; T --)
+    run(); return 0;
+}
